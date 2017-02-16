cd379fa3218939349a3e8950a24ef1a6bb1dfe67
lxi <lxi@uw.edu>
2017-02-15 20:17:58 -0800

    Added reference system project

---
+++ ReferenceSystem/README.md
@@ -0,0 +1,2 @@
+# CBIR Reference System
+Color histogram based CBIR system, CPU implementation
---
+++ ReferenceSystem/ReferenceSystem/main.cpp
@@ -0,0 +1,25 @@
+#define cimg_display 0
+
+#include <Windows.h>
+
+#include "CImg.h"
+
+using namespace std;
+using namespace cimg_library;
+
+int main(int argc, char *argv[])
+{
+	// Parameters:
+	//
+	// -u: If database folders do not exist, create them and quit; otherwise, update all image features and quit
+	// <filename>: search similar images to the given one
+
+	if (argc < 1)
+	{
+		
+	}
+	else
+	{
+		
+	}
+}
